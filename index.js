727df046d8980da69bc8b2f59018018ed12e4da2
Loirise <wieczorekszymon4@gmail.com>
2023-03-13 17:41:02 +0100

    description, added new nav link

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000
 
 app.engine('ejs', ejsmate);
-app.set('view enginge', 'ejs');
+app.set('view engine', 'ejs');
 
 app.set('views', path.join(__dirname, '/views'));
 app.use(express.static(path.join(__dirname, 'public')));
